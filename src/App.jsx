8d13b632dc4ec1ddb05879dced9e1dfd69b3d446
“Ragy4a” <ragy4a@gmail.com>
2024-05-26 12:38:18 +0300

    Added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 import './App.css'
+import Movies from './components/Movies/Movies';
+import Actors from './components/Actors/Actors';
+import Directors from './components/Directors/Directors';
+import Studios from './components/Studios/Studios';
+import HomePage from './components/HomePage/HomePage'
+import Layout from './components/Layout';
 
 function App() {
-
   return (
-    <>
-      Hello
-    </>
+    <Router>
+      <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/movies/*' element={<Movies />} />
+            <Route path='/actors/*' element={<Actors />} />
+            <Route path='/directors/*' element={<Directors />} />
+            <Route path='/studios/*' element={<Studios />} />
+            <Route index element={<HomePage />} />
+            <Route path='*' element={<Navigate to='/movies' replace />}/>
+          </Route>
+      </Routes>
+    </Router>
   )
 }
 
